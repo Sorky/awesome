befe7f6d59e201adce1076135907d5e91d1b3fd1
Sorky <43520033+Sorky@users.noreply.github.com>
2019-01-21 20:57:27 +1100

    Update utils_spec.lua

--- spec/menubar/utils_spec.lua
+++ spec/menubar/utils_spec.lua
@@ -15,7 +15,7 @@ describe("menubar.utils lookup_icon_uncached", function()
     local icon_theme
 
     local function assert_found_in_path(icon, path)
-        assert.matches(path, utils.lookup_icon_uncached(icon) or '')
+        assert.matches(path .. "$", utils.lookup_icon_uncached(icon) or '')
     end
 
     setup(function()
